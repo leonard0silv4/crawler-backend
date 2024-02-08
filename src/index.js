97a304c34405e2ae2d3eac1d316e3477639bb2d5
Leonardo Silva <lorsilva.fcamara@rd.com.br>
2024-02-08 16:58:01 -0300

    es6

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import express from "express";
 import 'dotenv/config'
 import mongoose from 'mongoose';
 import cors from 'cors';
-// const path = require("path");
 
 import routes from './routes.js';
 
 const app = express();
 
 mongoose.connect(
-  // "mongodb+srv://rafael_:manu18gabi21@omnistack.ww2gz.mongodb.net/AGMATOS?retryWrites=true&w=majority",
   `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_STRING}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
@@ -17,25 +15,13 @@ mongoose.connect(
   }
 );
 
-// GET, POST, PUT, DELETE
 
-// req.query = acessar query params url req.query.id
-// req.params = acessar route params app.put('usr/:id') => req.params.id
-
-// app.use(cors({ origin: "http://192.168.0.12" }));
 app.use(cors());
-// app.options("*", cors());
+
 app.use(express.json());
 app.use(routes);
-// app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
-
 
 var porta = process.env.PORT || 3333;
-
-// app.get('/', (req, res) =>{
-//   res.send('hre')
-// })
-
 
 
 app.listen(porta, () => {
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import hbs from 'nodemailer-express-handlebars'
+import path from 'path';
+
 
 
 import LinkController from './controllers/LinkController.js'
@@ -10,8 +14,62 @@ const routes = express.Router();
 routes.get("/",  (req, res) => {
     res.json({status : new Date()});
 });
-routes.get("/healthz",  (req, res) => {
-    res.status(200).json({status : 'ok'});
+
+
+
+routes.get("/healthz",  async (req, res) => {
+
+    const user = {
+        name : 'Gabriela Silva',
+        email : 'agabzinha.silva@gmail.com'
+    }
+
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+          
+        }
+      });
+
+      const handlebarOptions = {
+        viewEngine: {
+            partialsDir: path.resolve('./src/views'),
+            defaultLayout: false,
+        },
+        viewPath: path.resolve('./src/views/'),
+    };
+
+
+    transporter.use('compile', hbs(handlebarOptions))
+
+
+      const mailOptions = {
+        from: '"Leonardo R." <leo07vasp@gmail.com>', // sender address
+        template: "email", // the name of the template file, i.e., email.handlebars
+        to: user.email,
+        subject: `Welcome to My Company, ${user.name}`,
+        context: {
+          name: user.name,
+          company: 'my company'
+        },
+      };
+
+      transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+            console.log(err)
+            res.status(200).json({error : 'fail'});
+        } else {
+            console.log(data)
+            res.status(200).json({status : 'ok'});
+        }
+      });
+
+
+
+    
 });
 
 routes.get("/links",verifyJWT.isTokenized, LinkController.index);
